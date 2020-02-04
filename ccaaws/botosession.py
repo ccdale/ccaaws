2b028f5e6b92b9526fdabca829f9b9009b3a2a69
ccdale <chris.allison@hivehome.com>
2020-02-04 07:27:30 +0000

    add region detection

--- ccaaws/botosession.py
+++ ccaaws/botosession.py
@@ -23,6 +23,7 @@ class BotoSession:
             accesskey - the aws access key id to use (along with the secret key).
             secretkey - the aws secret access key to use (along with the access key id).
             stoken - the aws session token to use
+            region - the aws region
 
         Environment Variables (if set override the default credentials):
             AWS_ACCESS_KEY_ID
@@ -31,14 +32,19 @@ class BotoSession:
         """
         self.client = None
         self.profile = None
+        self.region = None
         self.usekeys = False
         self.kwargs = None
         self.usedefault = True
         if len(kwargs) > 0:
+            if "region" in kwargs:
+                self.region = kwargs["region"]
             if "profile" in kwargs:
                 self.profile = kwargs["profile"]
             elif "accesskey" in kwargs and "secretkey" in kwargs:
                 self.kwargs = {}
+                if self.region is not None:
+                    self.kwargs["region_name"] = self.region
                 self.kwargs["aws_access_key_id"] = kwargs["accesskey"]
                 self.kwargs["aws_secret_access_key"] = kwargs["secretkey"]
                 if "stoken" in kwargs:
@@ -51,14 +57,23 @@ class BotoSession:
 
     def newSession(self):
         if self.profile is None:
-            return boto3.session.Session()
+            if self.region is None:
+                return boto3.session.Session()
+            else:
+                return boto3.session.Session(region_name=self.region)
         else:
-            return boto3.session.Session(profile_name=self.profile)
+            if self.region is None:
+                return boto3.session.Session(profile_name=self.profile)
+            else:
+                return boto3.session.Session(
+                    profile_name=self.profile, region_name=self.region
+                )
 
     def newClient(self, service="iam"):
         try:
             if self.usekeys:
-                self.client = boto3.client(service, **self.kwargs)
+                if self.region is None:
+                    self.client = boto3.client(service, **self.kwargs)
             else:
                 session = self.newSession()
                 self.client = session.client(service)
